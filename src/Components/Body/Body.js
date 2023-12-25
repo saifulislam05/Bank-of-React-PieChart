7523c6035aff92ecf6236b75ab1c7aba4d69ad6f
saifulislam05 <saiful.bijulia@gmail.com>
2023-12-25 22:55:15 +0530

    Added Result PieChart

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,6 +1,7 @@
 
 import React, { useState } from "react";
 import Ranges from "./Ranges";
+import Result from "./Result";
 import TenureSelect from "./TenureSelect";
 
 
@@ -13,10 +14,13 @@ const Body = () => {
    interestRate: 5,
  });
   return (
-    <div className="w-11/12 mx-auto flex flex-col justify-center px-5 py-2 lg:px-auto lg:pt-12">
+    <div className="w-11/12 mx-auto flex flex-wrap justify-between px-5 py-2 lg:px-auto lg:pt-12">
       <div className="w-full md:w-5/12">
         <Ranges data={data} setData={setData} />
         <TenureSelect data={data} setData={setData} />
+      </div>
+      <div className="w-full md:w-6/12 flex justify-center items-center">
+        <Result data={data}/>
       </div>
     </div>
   );
